55b9ed95e2f9d9ed51bae5dc0b1a13303b5e7c45
Joseph Davis <jdavi195@gmail.com>
2022-08-09 15:52:16 -0400

    fixes

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ $("p").mouseenter(function(){
     $(this).hide();
 })
 
-$()
+$(".menu").click(function(){
+    $(this).find(".menu").toggleClass("active");
+    $(this).siblings(".toggle").find(".menu").removeClass("active");
+  });
+
+const modal = document.querySelector("modal");
+const previews = document.querySelectorALL(".img");
+const original  = document.querySelector(".full-img");
