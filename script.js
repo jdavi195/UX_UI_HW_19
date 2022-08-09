4d8955f8753f5ba45d8f605169278bbe9c81904e
Joseph Davis <jdavi195@gmail.com>
2022-08-09 18:14:24 -0400

    push

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-$("p").hover(function(){
-    $(this).hide();
-})
-
-$(".menu").click(function(){
-    $(this).find(".menu").toggleClass("active");
-    $(this).siblings(".toggle").find(".menu").removeClass("active");
-  });
-
-const modal = document.querySelector("modal");
-const previews = document.querySelectorALL(".img");
-const original  = document.querySelector(".full-img");
-
-previews.forEach(preview => {
-    preview.addEventListener("mouseover", () => {
-        modal.classList.add("open");
-    })
-
-})
-
 modal.addEventListener("click", (e) => {
     console.log(e);
 });
@@ -32,7 +12,3 @@ function setOldImage()
         ="images/budgetcover.png";
         //alert();
 }
-
-<images id="" onmouseover="setNewImage()"
-onmouseout="setOldImage()" src="images/budgetcover.png"
-width="300"/>
